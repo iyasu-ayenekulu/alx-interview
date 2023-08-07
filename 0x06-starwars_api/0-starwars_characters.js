a53afa65bd4dbcd0f4b36eb4dc97b4f648081386
josh <iyasuayenekulu3@gmail.com>
2023-08-07 11:54:23 -0700

    done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function getRequestStar (charactersUrl) {
+  return new Promise((resolve, reject) => {
+    request(charactersUrl, (error, response, body) => {
+      if (error) reject(error);
+      resolve(JSON.parse(body));
+    });
+  });
+}
+
+async function charNameStarWars () {
+  const response = await getRequestStar(url);
+  for (let i = 0; i < response.characters.length; i++) {
+    const char = await getRequestStar(response.characters[i]);
+    console.log(char.name);
+  }
+}
+
+charNameStarWars();
